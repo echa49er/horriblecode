37c99aca817e62aa64b7a3fb0ca5b0d62b378123
echan49er <bechaniq@charlotte.edu>
2025-06-24 23:07:17 -0400

    Main class for testing badcalculator.py functionality

---
+++ badmain.py
@@ -0,0 +1,8 @@
+import badcalculator
+
+guh = badcalculator.Calculator(1, 1, 1, 1, 1) #Creates calculator object
+
+guh.uhh()
+guh.uhmmmm()
+guh.yeahsure()
+guh.thisnameissoooolongijustwastedyourtimemakingyoureadallofthis()
